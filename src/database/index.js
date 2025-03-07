a8d3e58408eb176eb38e278426c0fb795cacd515
dsjramesh7 <rameshdussa07@gmail.com>
2025-03-07 17:31:31 +0530

    practice

--- src/database/index.js
+++ src/database/index.js
@@ -1,3 +1,5 @@
 import { PASSWORD, USERNAME } from "@/utils/constants";
 
-const connectToDB = `mongodb+srv://${USERNAME}:${PASSWORD}@authclusterapp.tvet0.mongodb.net/`;
+const connectToDB = async () => {
+  const connectionURL = `mongodb+srv://${USERNAME}:${PASSWORD}@authclusterapp.tvet0.mongodb.net/`;
+};
