9c964c2dffcb5e1e8b9e1711607a3af839adbf45
dsjramesh7 <rameshdussa07@gmail.com>
2025-03-09 00:41:38 +0530

    practice

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,10 @@ import { PASSWORD, USERNAME } from "@/utils/constants";
 import * as mongoose from "mongoose";
 
 const connectToDB = async () => {
-  const connectionURL = `mongodb+srv://${USERNAME}:${PASSWORD}@authclusterapp.kslo1.mongodb.net/`;
+  const url = `mongodb+srv://${USERNAME}:${PASSWORD}@authclusterapp.kslo1.mongodb.net/`;
   mongoose
-    .connect(connectionURL)
-    .then(() => console.log("Auth Database Connected Successfully"))
+    .connect(url)
+    .then(() => console.log("DataBase Connected Sucessfully"))
     .catch((error) => console.log(error));
 };
 
